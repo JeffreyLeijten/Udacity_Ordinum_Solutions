c3e089c6a33aed9672718774cca7cb6aa2318bb5
Jeffrey Leijten <jeffreyleijten@live.nl>
2017-04-12 16:34:20 +0200

    Add intents to information

--- app/src/main/java/com/example/android/ordinumsolutions/MainActivity.java
+++ app/src/main/java/com/example/android/ordinumsolutions/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.ordinumsolutions;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToLocation(View view){
+        Uri location = Uri.parse("geo:0,0?q="+getString(R.string.address));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void openWebsiste(View view){
+        Uri webpage = Uri.parse("http:"+getString(R.string.website));
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (webIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(webIntent);
+        }
+    }
+
+    public void sendMail(View view){
+        Intent eMailIntent = new Intent(Intent.ACTION_SENDTO);
+        eMailIntent.setData(Uri.parse("mailto:"));
+        String[] addresses = new String[1];
+        addresses[0] = getString(R.string.e_mail_address);
+        eMailIntent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        if (eMailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(eMailIntent);
+        }
+    }
+
 }
